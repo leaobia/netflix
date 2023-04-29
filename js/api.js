216dfc68be36702975b27af263a2f17ae14f1bf9
unknown <biancapeleao@gmail.com>
2023-04-29 10:52:00 -0300

    atualizacoes

--- js/api.js
+++ js/api.js
@@ -1,16 +1,20 @@
 'use strict'
 
+const fetchAPI = () => {
 
+  const fetchDados = fetch('https://raw.githubusercontent.com/prust/wikipedia-movie-data/master/movies.json')
+    .then(response => response.json())
+    .then(data => {
+      var titles = data.map(movie => movie.title).slice(0, 2).sort(() => Math.random() - 0.5);
 
-const testando =  () => {
-
-    const batman = ['Barbie', 'Batman', 'Vingadores']
-    batman.forEach((item) =>{
+      titles.forEach((item) => {
         fetch(`https://www.omdbapi.com/?apikey=d2feea&t=${item}`)
-        .then(result => result.json())
-        .then(json => console.log(json))
-    }
-   
-)}
+          .then(result => result.json())
+          .then(json => {
+            console.log(json);
+          });
+      })
+    })
+}
 
-testando()
+fetchAPI()
--- js/router.js
+++ js/router.js
@@ -1,20 +1,26 @@
 'use strict'
 
 const routes = {
-    '/': '/pages/series.html'
+    '/' : '/pages/home.html',
+    '/vermelho' : '/pages/vermelho.html',
+    '/azul' : '/pages/azul.html',
+    '/roxo' : '/pages/roxo.html',
+    '/laranja' : '/pages/laranja.html'
 }
 
-export const route = async() => {
+const route = async ()  => {
     window.event.preventDefault()
-    window.history.pushState({}, "", window.event.target.href)
+    window.history.pushState({}, "" , window.event.target.href)
+    const path = window.location.pathname
+    const response = await fetch(routes[path])
+    const html = await response.text()
+    const root = document.getElementById('root').innerHTML = html
 
-    const path = window.location.pathname;
-
-    const response = await fetch(routes[path]);
-    const html = await response.text();
-
-    document.getElementById('root').innerHTML = html
-    console.log(html);
 }
+
+
+
+
+
 
 window.route = route
